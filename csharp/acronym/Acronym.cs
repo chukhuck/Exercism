96c00172f3821cdc1048ca081372b954ce43d5c1
Кузнецов Александр <aka.kuznec@ya.ru>
2019-04-30 15:46:20 +0300

    cs.acronym.first_submit

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        char[] denySymbols = {',', '\'', '_', '.'};
+        char[] separators = {' ', '-'};
+
+        return string.Join("", phrase
+                                    .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(
+                                        word => word.ToUpper().TrimStart(denySymbols).First())); 
+    }
+}
