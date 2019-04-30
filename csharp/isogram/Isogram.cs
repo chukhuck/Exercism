d1e6c6d6c7e67bb6318c09e32f11a0a5142e0b96
Кузнецов Александр <aka.kuznec@ya.ru>
2019-04-30 17:42:30 +0300

    cs.isogram.submit1

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+            return true;
+
+        var cleanWord = word.Replace(" ", "").Replace("-", "").ToLower();
+
+        return cleanWord.GroupBy(letter => letter).Count() == cleanWord.Length;
+    }
+}
