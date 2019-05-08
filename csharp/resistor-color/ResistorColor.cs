38d24419e856814554b65851b5edca41283d0808
Кузнецов Александр <chuk777-89@mail.ru>
2019-05-09 00:50:58 +0300

    cs.resistorcolor.s1

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -5,25 +5,25 @@ public static class ResistorColor
 {
     public enum ResistorColors
     {
-        Black=0,
-        Brown=1,
-        Red=2,
-        Orange=3,
-        Yellow=4,
-        Green=5,
-        Blue=6,
-        Violet=7,
-        Grey=8,
-        White=9 
+        black=0,
+        brown=1,
+        red=2,
+        orange=3,
+        yellow=4,
+        green=5,
+        blue=6,
+        violet=7,
+        grey=8,
+        white=9 
     }
 
     public static int ColorCode(string color)
     {
-        return Colors().IndexAt(color);
+        return (int)Enum.Parse(typeof(ResistorColors), color);
     }
 
     public static string[] Colors()
     {
-        return new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+        return Enum.GetNames(typeof(ResistorColors));
     }
 }
