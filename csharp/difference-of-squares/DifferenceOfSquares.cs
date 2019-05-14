074a7f65e51300aa9aa6169befc656785a4b14ca
Кузнецов Александр <chuk777-89@mail.ru>
2019-05-14 11:05:53 +0300

    cs.differenceofsquares.s1

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -1,19 +1,37 @@
 ﻿using System;
+using System.Linq;
 
 public static class DifferenceOfSquares
 {
+    public static int CalculateSquareOfSum1(int max)
+    {
+        return (int)Math.Pow(Enumerable.Range(1,max).Sum(), 2);
+    }
+
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return (int)Math.Pow((1 + max)* max/2, 2);
     }
 
+    public static int CalculateSumOfSquares1(int max)
+    {
+        return Enumerable.Range(1,max).Select(i=>i*i).Sum();
+    }
+    
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return max * (max + 1) * (2*max + 1) / 6;
     }
 
-    public static int CalculateDifferenceOfSquares(int max)
+    public static int CalculateDifferenceOfSquares1(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int sumOfIntegers = max * (1 + max) / 2;
+
+        return (int)Math.Pow(sumOfIntegers, 2) - sumOfIntegers * (2*max + 1)/3;
+    }
+
+        public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
 }
