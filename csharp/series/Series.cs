6d116ed7c86568fb83c10083d23b53f8797affca
Кузнецов Александр <aka.kuznec@ya.ru>
2019-05-18 11:44:50 +0300

    cs.series.s1

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -4,6 +4,14 @@ public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (numbers.Length == 0 || sliceLength < 1 || numbers.Length  < sliceLength)
+            throw new ArgumentException("Invalid parameter");
+
+        string[] slices = new string[numbers.Length - sliceLength + 1];
+
+        for (int i = 0; i < numbers.Length - sliceLength + 1; i++)
+            slices[i] = numbers.Substring(i, sliceLength); 
+
+        return slices;
     }
 }
