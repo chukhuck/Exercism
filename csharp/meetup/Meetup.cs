6acdf3e0d6f950ccc2c9b154c00f1ce3b6c84388
Кузнецов Александр <aka.kuznec@ya.ru>
2019-05-19 21:29:40 +0300

    cs.meetup.s1

--- csharp/meetup/Meetup.cs
+++ csharp/meetup/Meetup.cs
@@ -2,19 +2,18 @@ using System;
 
 public enum Schedule
 {
-    Teenth,
-    First,
-    Second,
-    Third,
-    Fourth,
-    Last
+    Teenth = -1,
+    First = 0,
+    Second = 1,
+    Third = 2,
+    Fourth = 3,
+    Last = -2
 }
 
 public class Meetup
 {
-    private int _month;
-    private int _year;
-
+    private readonly int _month;
+    private readonly int _year;
 
     public Meetup(int month, int year)
     {
@@ -24,10 +23,39 @@ public class Meetup
 
     public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule)
     {
-        DateTime calculatedDate = new DateTime(_year, _month, 1);
+       switch (schedule)
+       {
+           case Schedule.First:
+           case Schedule.Second:
+           case Schedule.Third:
+           case Schedule.Fourth:
+                return GetFirstDayOfWeek(dayOfWeek).AddWeeks((int)schedule);
+           case Schedule.Last:
+                DateTime forth = GetFirstDayOfWeek(dayOfWeek).AddWeeks(3);
+                return DateTime.DaysInMonth(_year, _month) - forth.Day < 7 ? forth : forth.AddWeeks(1);
+           case Schedule.Teenth:
+                DateTime first = GetFirstDayOfWeek(dayOfWeek);
+                return first.Day < 6 ? first.AddWeeks(2) : first.AddWeeks(1);
+           default:
+                throw new ArgumentException("Invalid argument");
+       }
+    }
 
-        calculatedDate.
+    private DateTime GetFirstDayOfWeek(DayOfWeek dayOfWeek)
+    {
+        DateTime realDate = new DateTime(_year, _month, 1);
+        
+        while (realDate.DayOfWeek != dayOfWeek)
+            realDate = realDate.AddDays(1);
 
-        return calculatedDate;
+        return realDate;
+    }
+}
+
+public static class DateTimeExtension
+{
+    public static DateTime AddWeeks(this DateTime dt, int countOfWeek) 
+    {
+        return dt.AddDays(7 * countOfWeek);
     }
 }
