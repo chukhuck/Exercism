fb01a013ae6c757095a5fe2376da7ed1d796a6f7
Кузнецов Александр <aka.kuznec@ya.ru>
2019-05-20 16:38:23 +0300

    cs.armstrongnumbers.s1

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string numberAsString = number.ToString();
+
+        return number == numberAsString.Select(c=> Math.Pow(int.Parse(c.ToString()), numberAsString.Length)).Sum();
+    }
+}
