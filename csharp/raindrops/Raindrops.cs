7eea03e4553ee9e1b0e70a8071c560b895ef47ae
Кузнецов Александр <aka.kuznec@ya.ru>
2019-06-23 22:33:01 +0300

    cs.raindrops.s1

---
+++ csharp/raindrops/Raindrops.cs
@@ -0,0 +1,20 @@
+using System;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        string raindrops = "";
+
+        if (number % 3 == 0)
+            raindrops += "Pling";
+
+        if (number % 5 == 0)
+            raindrops += "Plang";
+
+        if (number % 7 == 0)
+            raindrops += "Plong";
+
+        return string.IsNullOrEmpty(raindrops) ? number.ToString() : raindrops;
+    }
+}
