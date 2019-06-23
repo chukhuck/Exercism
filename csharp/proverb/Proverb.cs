54f74f59e79055d84d73a14ca61827fb847c623a
Кузнецов Александр <aka.kuznec@ya.ru>
2019-06-23 23:41:45 +0300

    cs.proverb.s2

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -5,26 +5,15 @@ using System.Linq;
 
 public static class Proverb
 {
-    static List<string> song = new List<string>();
 
     public static string[] Recite(string[] subjects)
     {
-        song.Clear();
+        if (subjects.Length == 0)
+            return new string[0];
 
-        if (subjects.Length > 1)
-        {
-            for (int i = 0; i < subjects.Length - 1; i++)
-                song.Add($"For want of a {subjects[i]} the {subjects[i + 1]} was lost.");
-        }
-
-        if (subjects.Length != 0)
-        {
-            if(subjects.Contains("nail"))
-                song.Add("And all for the want of a nail.");
-            else
-                song.Add($"And all for the want of a {subjects.First()}.");
-        } 
-
-        return song.ToArray();
+        return subjects.
+                Zip(subjects.Skip(1), (x, y) => $"For want of a {x} the {y} was lost.").
+                Append($"And all for the want of a {subjects.First()}.").
+                ToArray();
     }
 }
