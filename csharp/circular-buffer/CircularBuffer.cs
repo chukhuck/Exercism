d00f3e32f03377670875af00bf51c43b32c4adf3
Кузнецов Александр <aka.kuznec@ya.ru>
2019-07-11 16:45:06 +0300

    cs.curcularbuffer.s1

---
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class CircularBuffer<T>
+{
+    T[] buffer;
+
+    int newItemPointer;
+
+    int length;
+
+    readonly int capacity;
+
+    public CircularBuffer(int capacity)
+    {
+        this.capacity = capacity;
+        buffer = new T[capacity];
+    }
+
+    public T Read()
+    {
+        if (length == 0)
+            throw new InvalidOperationException();
+
+        int oldestItemPointer = (newItemPointer - length + capacity) % capacity;
+        length--;
+
+        return buffer[oldestItemPointer];
+    }
+
+    public void Write(T value)
+    {
+        if(length == capacity)
+            throw new InvalidOperationException();
+
+        buffer[newItemPointer] = value;
+
+        length++;
+        newItemPointer = (newItemPointer + 1) % capacity;
+    }
+
+    public void Overwrite(T value)
+    {
+        buffer[newItemPointer] = value;
+        newItemPointer = (newItemPointer + 1) % capacity;
+
+        if (length < capacity)
+            length++;
+    }
+
+    public void Clear()
+    {
+        length = 0;
+    }
+}
