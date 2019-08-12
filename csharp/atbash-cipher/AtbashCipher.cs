ed30151087e7befe99f62b7295e22431962386cb
aka.kuznec <aka.kuznec@yandex.ru>
2019-08-12 14:32:13 +0300

    cs.atbashcipher.s1

--- csharp/atbash-cipher/AtbashCipher.cs
+++ csharp/atbash-cipher/AtbashCipher.cs
@@ -1,14 +1,65 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class AtbashCipher
 {
-    public static string Encode(string plainValue)
+    public static ISubstitutionCipher cipher;
+
+    public static string Encode(string plainText)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        cipher = new MyAtbashCipher();
+
+        var cipherText = string.Concat( plainText
+                                .ToLower()
+                                .Where(char.IsLetterOrDigit)
+                                .Select(l => char.IsLetter(l) ? cipher.Encode(l) : l)); 
+
+        return string.Concat(GroupCipheredText(cipherText, ' ', 5));
     }
 
-    public static string Decode(string encodedValue)
+    public static string Decode(string cipheredText)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        cipher = new MyAtbashCipher();
+        return string.Concat( cipheredText
+                                .ToLower()
+                                .Where(char.IsLetterOrDigit)
+                                .Select(l => char.IsLetter(l) ? cipher.Decode(l) : l));      
+    }
+
+    private static IEnumerable<char> GroupCipheredText(IEnumerable<char> cipherText, char splitter, int countSymbolsInGroup)
+    {
+        int count = cipherText.Count();
+        for (int i = 0; i < count; i++)
+        {
+            yield return cipherText.ElementAt(i);
+
+            if(i % countSymbolsInGroup == countSymbolsInGroup - 1 && i != count - 1)
+                yield return splitter;
+        }
     }
 }
+
+public interface ISubstitutionCipher
+{
+    char Encode(char letter);
+
+    char Decode(char letter);
+}
+
+internal class MyAtbashCipher : ISubstitutionCipher
+{
+    public readonly int LenghtOfAlphabet = 26;
+
+    public char Decode(char letter)
+    {
+        return (char)(LenghtOfAlphabet - 1 - (letter - 'a') + 'a');
+    }
+
+    public char Encode(char letter)
+    {
+        return (char)(LenghtOfAlphabet - 1 - (letter - 'a') + 'a');
+    }
+}
+
+
