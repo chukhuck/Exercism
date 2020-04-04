0eba7f245e161ea830ba0baea242c1a3b21d28bb
aka.kuznec <aka.kuznec@yandex.ru>
2020-04-04 21:56:02 +0300

    1

--- csharp/saddle-points/SaddlePoints.cs
+++ csharp/saddle-points/SaddlePoints.cs
@@ -6,7 +6,6 @@ public static class SaddlePoints
 {
     public static IEnumerable<(int, int)> Calculate(int[,] matrix)
     {
-        
-        
+        throw new NotImplementedException("You need to implement this function.");
     }
 }
