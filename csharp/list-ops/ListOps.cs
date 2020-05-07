d04156f84e84831f6b2eccac539508822647799d
aka.kuznec <aka.kuznec@yandex.ru>
2020-05-07 15:47:29 +0300

    cs.listops.s2

--- csharp/list-ops/ListOps.cs
+++ csharp/list-ops/ListOps.cs
@@ -4,43 +4,35 @@ using System.Linq;
 
 public static class ListOps
 {
-    public static int Length<T>(List<T> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int Length<T>(List<T> input) => Foldl(input, 0, (count, _) => count + 1);
 
     public static List<T> Reverse<T>(List<T> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    => Foldl(input, new List<T>(), (list , item) => Append(new List<T>(){item}, list));
 
     public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    => Foldl(input, new List<TOut>(), (list , item) => Append(list, new List<TOut>(){map(item)}));
 
     public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    => Foldl(input, new List<T>(), (list, item) => predicate(item) ? Append(list, new List<T>(){item}) : NoAction(list));
 
     public static TOut Foldl<TIn, TOut>(List<TIn> input, TOut start, Func<TOut, TIn, TOut> func)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        TOut temp = start;
+
+        for (int i = 0; i < input.Count; i++)
+            temp = func(temp, input[i]);
+
+        return temp; 
     }
 
     public static TOut Foldr<TIn, TOut>(List<TIn> input, TOut start, Func<TIn, TOut, TOut> func)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    => Foldl(Reverse(input), start, (val, i) => func(i, val));
 
     public static List<T> Concat<T>(List<List<T>> input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    => Foldl(input, new List<T>(), (commonList, subList) => Append(commonList, subList));
 
     public static List<T> Append<T>(List<T> left, List<T> right)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    => Foldl(right, new List<T>(left), (list, item) => list.Append(item).ToList());
+
+    public static List<T> NoAction<T>(List<T> input) => input;
 }
--- csharp/test/Program.cs
+++ csharp/test/Program.cs
@@ -8,23 +8,98 @@ namespace test
     {
         static void Main(string[] args)
         {
-            foreach (var item in CountWords("Joe can't tell between 'large' and large."))
-            {
-                Console.WriteLine($"{item.Key}-{item.Value}");
-            }
-            
+            var list = new List<int> { 2, 5 };
+            var initial = 5;
+            var function = new Func<int, int, int>((x, y) => x / y);
+            Console.WriteLine(Foldr(list, initial, function));
         }
 
-        public static IDictionary<string, int> CountWords(string phrase)
+        public static int Length<T>(List<T> input)
+    {
+        return input.Count;
+    }
+
+    public static List<T> Reverse<T>(List<T> input)
+    {
+        List<T> temp = new List<T>(input.Count);
+
+        for (int i = input.Count - 1; i > -1; i--)
         {
-            string[] separators = new string[] { " ", ",", "\n", };
-            char[] trimmedChars = new char[] { '!', '&', '@', '$', ':', '%', '^', '\'', '.' };
-
-            return phrase.Split(separators, StringSplitOptions.RemoveEmptyEntries)
-                  .Select(word => word.Trim(trimmedChars).ToLower())
-                  .GroupBy(word => word)
-                  .ToDictionary(groupedWord => groupedWord.Key, groupedWord => groupedWord.Count());
+            temp.Add(input[i]);
         }
+
+        return temp;
+    }
+
+    public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map)
+    {
+        List<TOut> temp = new List<TOut>(input.Count);
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            temp.Add(map(input[i]));
+        }
+
+        return temp;
+    }
+
+    public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate)
+    {
+        List<T> temp = new List<T>(input.Count);
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            if (predicate(input[i]))
+                temp.Add(input[i]);
+        }
+
+        return temp;
+    }
+
+    public static TOut Foldl<TIn, TOut>(List<TIn> input, TOut start, Func<TOut, TIn, TOut> func)
+    {
+        TOut temp = start;
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            temp = func(temp, input[i]);
+        }
+
+        return temp; 
+    }
+
+    public static TOut Foldr<TIn, TOut>(List<TIn> input, TOut start, Func<TIn, TOut, TOut> func)
+    {
+        TOut temp = start;
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            temp = func(input[i], temp);
+        }
+
+        return temp; 
+    }
+
+    public static List<T> Concat<T>(List<List<T>> input)
+    {
+        List<T> temp = new List<T>();
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            temp.AddRange(input[i]);
+        }
+
+        return temp;
+    }
+
+    public static List<T> Append<T>(List<T> left, List<T> right)
+    {
+        List<T> temp = new List<T>(left.Count + right.Count);
+        temp.AddRange(left);
+        temp.AddRange(right);
+
+        return temp;
+    }
 
     }
 }
