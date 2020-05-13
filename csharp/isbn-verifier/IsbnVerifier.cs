fb3d8e4c918efa2eeb4c7191de85a7c19efa1951
aka.kuznec <aka.kuznec@yandex.ru>
2020-05-13 11:30:42 +0300

    cs.isbnverivier.s1

--- csharp/isbn-verifier/IsbnVerifier.cs
+++ csharp/isbn-verifier/IsbnVerifier.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public static class IsbnVerifier
 {
     public static bool IsValid(string number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Regex isbnPattern = new Regex(@"^\d-?\d{3}-?\d{5}-?[\dX]$");
+
+        if (!isbnPattern.IsMatch(number))
+            return false;
+
+        int sum = number.Replace("-", "")
+                     .Replace("X", "A")
+                     .Select((c, idx) => Convert.ToInt32(c.ToString(), 16) * (10 - idx))
+                     .Sum();
+
+        return sum % 11 == 0;
+
+
     }
 }
