c42cc0b9504c3c8f165dacc640d7462e9c7b4180
aka.kuznec <aka.kuznec@yandex.ru>
2020-05-14 19:45:15 +0300

    cs.romannumbers.s1

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
 
 public static class RomanNumeralExtension
 {
+    static Dictionary<int, string> ra = new Dictionary<int, string>
+        { { 1000, "M" },  { 900, "CM" },  { 500, "D" },  { 400, "CD" },  { 100, "C" },
+                          { 90 , "XC" },  { 50 , "L" },  { 40 , "XL" },  { 10 , "X" },
+                          { 9  , "IX" },  { 5  , "V" },  { 4  , "IV" },  { 1  , "I" } };
+
     public static string ToRoman(this int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var temp = ra.Where(d => value >= d.Key).FirstOrDefault();
+        return temp.Key == 0 ? "" : temp.Value + ToRoman(value - temp.Key);
     }
 }
--- csharp/test/Program.cs
+++ csharp/test/Program.cs
@@ -8,98 +8,24 @@ namespace test
     {
         static void Main(string[] args)
         {
-            var list = new List<int> { 2, 5 };
-            var initial = 5;
-            var function = new Func<int, int, int>((x, y) => x / y);
-            Console.WriteLine(Foldr(list, initial, function));
+            Console.WriteLine(1024.ToRoman());
         }
-
-        public static int Length<T>(List<T> input)
-    {
-        return input.Count;
     }
 
-    public static List<T> Reverse<T>(List<T> input)
+    public static class RomanExtension
     {
-        List<T> temp = new List<T>(input.Count);
+        static Dictionary<int, string> ra = new Dictionary<int, string>
+        { { 1000, "M" },  { 900, "CM" },  { 500, "D" },  { 400, "CD" },  { 100, "C" },
+                          { 90 , "XC" },  { 50 , "L" },  { 40 , "XL" },  { 10 , "X" },
+                          { 9  , "IX" },  { 5  , "V" },  { 4  , "IV" },  { 1  , "I" } };
 
-        for (int i = input.Count - 1; i > -1; i--)
+        public static string ToRoman(this int value)
         {
-            temp.Add(input[i]);
+            var temp = ra.Where(d => value >= d.Key).FirstOrDefault();
+            Console.WriteLine(temp);
+            Console.WriteLine(temp.Key);
+            Console.WriteLine(temp.Value);
+            return temp.Key == 0 ? "" : temp.Value + ToRoman(value - temp.Key);
         }
-
-        return temp;
-    }
-
-    public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map)
-    {
-        List<TOut> temp = new List<TOut>(input.Count);
-
-        for (int i = 0; i < input.Count; i++)
-        {
-            temp.Add(map(input[i]));
-        }
-
-        return temp;
-    }
-
-    public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate)
-    {
-        List<T> temp = new List<T>(input.Count);
-
-        for (int i = 0; i < input.Count; i++)
-        {
-            if (predicate(input[i]))
-                temp.Add(input[i]);
-        }
-
-        return temp;
-    }
-
-    public static TOut Foldl<TIn, TOut>(List<TIn> input, TOut start, Func<TOut, TIn, TOut> func)
-    {
-        TOut temp = start;
-
-        for (int i = 0; i < input.Count; i++)
-        {
-            temp = func(temp, input[i]);
-        }
-
-        return temp; 
-    }
-
-    public static TOut Foldr<TIn, TOut>(List<TIn> input, TOut start, Func<TIn, TOut, TOut> func)
-    {
-        TOut temp = start;
-
-        for (int i = 0; i < input.Count; i++)
-        {
-            temp = func(input[i], temp);
-        }
-
-        return temp; 
-    }
-
-    public static List<T> Concat<T>(List<List<T>> input)
-    {
-        List<T> temp = new List<T>();
-
-        for (int i = 0; i < input.Count; i++)
-        {
-            temp.AddRange(input[i]);
-        }
-
-        return temp;
-    }
-
-    public static List<T> Append<T>(List<T> left, List<T> right)
-    {
-        List<T> temp = new List<T>(left.Count + right.Count);
-        temp.AddRange(left);
-        temp.AddRange(right);
-
-        return temp;
-    }
-
     }
 }
