050d5900ed909783a36e17c8184682efeb01134f
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-04-19 11:55:42 +0300

    Solse Python.Leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,11 @@
 def leap_year(year):
-    pass
+    if (year % 400 == 0):
+        return True
+
+    if(year % 100 == 0):
+        return False
+
+    if(year % 4 == 0):
+        return True
+
+    return False
