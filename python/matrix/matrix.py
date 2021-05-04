cfd77aa50b17187be6f8309aba8ef240bd6606f6
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-05-04 22:46:35 +0300

    python.matrix.s2

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,14 +1,12 @@
 class Matrix:
     def __init__(self, matrix_string: str):
-        self.row_lenght = 0
         self.matrix_list = []
 
         for row in list(map(str.split, matrix_string.splitlines())):
-            self.matrix_list.extend(list(map(int, row)))
-            self.row_lenght = len(row)
+            self.matrix_list.append(list(map(int, row)))
 
-    def row(self, index: int) -> list:
-        return self.matrix_list[self.row_lenght * (index - 1):self.row_lenght * index]
+    def row(self, index: int) -> list():
+        return self.matrix_list[index - 1]
 
-    def column(self, index: int) -> list:
-        return self.matrix_list[index - 1::self.row_lenght]
+    def column(self, index: int) -> list():
+        return [row[index - 1] for row in self.matrix_list]
