33cc4ad32e6c6a70fcebbebef04a273701e9b070
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-05-04 23:11:22 +0300

    python.matrix.s3

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,12 +1,10 @@
 class Matrix:
     def __init__(self, matrix_string: str):
-        self.matrix_list = []
-
-        for row in list(map(str.split, matrix_string.splitlines())):
-            self.matrix_list.append(list(map(int, row)))
+        self.matrix = [list(map(int, row.split()))
+                       for row in matrix_string.splitlines()]
 
     def row(self, index: int) -> list():
-        return self.matrix_list[index - 1]
+        return self.matrix[index - 1]
 
     def column(self, index: int) -> list():
-        return [row[index - 1] for row in self.matrix_list]
+        return [row[index - 1] for row in self.matrix]
