d382e266adb0405e9ab79966ccd92bca85fa76de
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-09 21:22:09 +0300

    python.perfectnumbers.s1

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,15 @@
 def classify(number):
-    pass
+    if number < 1:
+        raise ValueError('the Number must be greater than 1.')
+
+    summary = sum(get_factors(number))
+
+    if summary == number:
+        return 'perfect'
+    elif summary > number:
+        return 'abundant'
+    else:
+        return 'deficient'
+
+def get_factors(number):
+    return [i for i in range(1, number//2 + 1) if number%i == 0]
