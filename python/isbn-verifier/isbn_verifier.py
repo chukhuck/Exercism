cd2fb89f387d7979b7f6e3e3af9b53cbe0157efc
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-09 17:09:37 +0300

    python.isbnverifier.s0

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,6 @@
-def is_valid(isbn):
-    pass
+
+
+def is_valid(isbn: str) -> bool:
+    clear_ISBN_in_hex = isbn.replace('-', '').replace('X', 'A')
+    summary = sum([ int(i, base=16) * ( 10 - index) for i in clear_ISBN_in_hex]) 
+    return summary % 11 
