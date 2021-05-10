10f9a68a75b010e49f148c9efd96a17fd1f11245
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-10 16:53:43 +0300

    python.series.s2

--- python/series/series.py
+++ python/series/series.py
@@ -1,5 +1,5 @@
 def slices(series, length):
     if len(series) < length or length < 1:
-        raise ValueError('.+')
+        raise ValueError('Invavid arguments.')
 
     return [series[i:i+length] for i in range(len(series) - length + 1)]
