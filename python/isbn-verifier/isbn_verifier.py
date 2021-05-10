5a3800998f2b3b1211c5906c97c8c9a4bf6836ac
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-10 12:47:55 +0300

    python.isbnverifier.s1

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,6 +1,11 @@
-
+import re
 
 def is_valid(isbn: str) -> bool:
+    isbn_pattern = r'^\d-?\d{3}-?\d{5}-?[\dX]$'
+
+    if not re.match(isbn_pattern, isbn):
+        return False
+
     clear_ISBN_in_hex = isbn.replace('-', '').replace('X', 'A')
-    summary = sum([ int(i, base=16) * ( 10 - index) for i in clear_ISBN_in_hex]) 
-    return summary % 11 
+    summary = sum([int(value, base=16) * ( 10 - index) for index, value in enumerate(clear_ISBN_in_hex)]) 
+    return summary % 11 == 0
