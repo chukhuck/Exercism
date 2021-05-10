ad5cdf6b84412beb69b4c78c5a79234b28e062c4
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-10 14:20:56 +0300

    python.phonenumber.s1

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,3 +1,18 @@
+import re
+
 class PhoneNumber:
-    def __init__(self, number):
-        pass
+    number_pattern = r'^[2-9]\d\d[2-9]\d{5}'
+
+    def __init__(self, number: str):
+        self.number = ''.join([c for c in number if c.isdigit()])
+
+        if len(self.number) == 11 and self.number[0] == '1':
+            self.number = self.number[1:]
+
+        if not re.match(PhoneNumber.number_pattern, self.number) or len(self.number) > 10:
+            raise ValueError('The invalid argument.')
+
+        self.area_code = self.number[:3]
+
+    def pretty(self) -> str:
+        return '('+self.number[:3]+')-'+self.number[3:6]+'-'+self.number[-4:]
