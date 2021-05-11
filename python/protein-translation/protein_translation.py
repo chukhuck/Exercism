cf6aa13121e3bb932af00518bb0b14c0f2608d1f
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-11 21:20:03 +0300

    python.proteintranslation.s1

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,16 @@
 def proteins(strand):
-    pass
+    encoding_table = {
+    'AUG':'Methionine',
+    'UUU':'Phenylalanine', 'UUC':'Phenylalanine',
+    'UUA':'Leucine', 'UUG':'Leucine',
+    'UCU':'Serine', 'UCC':'Serine', 'UCA':'Serine', 'UCG':'Serine',
+    'UAU':'Tyrosine', 'UAC':'Tyrosine',
+    'UGU':'Cysteine', 'UGC':'Cysteine',
+    'UGG':'Tryptophan',
+    'UAA':'STOP', 'UAG':'STOP', 'UGA':'STOP'}
+
+
+    full_strand = [encoding_table[strand[i:i+3:]] for i in range(len(strand))[::3]]
+    stop_index = full_strand.index('STOP') if 'STOP' in full_strand else len(full_strand)
+
+    return full_strand[:stop_index]
