e07c6f377011f9854c313eac80a04396c4a9a676
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-13 12:55:00 +0300

    python.banlaccount.s1

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,18 +1,53 @@
+import threading
+
 class BankAccount:
+    
     def __init__(self):
-        pass
+        self.is_open = False
+        self.balance = 0.0
+        self.lock = threading.Lock()
+
 
     def get_balance(self):
-        pass
+        if not self.is_open:
+            raise ValueError('The account is not openned')
+
+        return self.balance
 
     def open(self):
-        pass
+        if self.is_open:
+            raise ValueError('The account is openned yet.')
+        else:
+            self.is_open = True
 
     def deposit(self, amount):
-        pass
+        if not self.is_open:
+            raise ValueError('The account is not openned')
+
+        if amount < 0:
+            raise ValueError('Incorrect value')
+
+        self.lock.acquire()
+        self.balance = self.balance + amount
+        self.lock.release()
 
     def withdraw(self, amount):
-        pass
+        if not self.is_open:
+            raise ValueError('The account is not openned')
+
+        if self.balance < amount:
+            raise ValueError('Sorry...you dont have that money.')
+
+        if amount < 0:
+            raise ValueError('Incorrect value')
+
+        self.lock.acquire()
+        self.balance = self.balance - amount
+        self.lock.release()
 
     def close(self):
-        pass
+        if self.is_open:
+            self.is_open = False
+            self.balance = 0.0
+        else:
+            raise ValueError('The account is closed yet.')
