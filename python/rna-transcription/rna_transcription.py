4c4aca28c7752d9d10568f91f7b59a46c37be12c
aka.kuznec <aka.kuznec@yandex.ru>
2021-05-14 09:56:35 +0300

    python.rnatranscription.s1

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,3 @@
-def to_rna(dna_strand):
-    pass
+def to_rna(dna_strand:str) ->str:
+    encoding_table = {'G':'C', 'C':'G','A':'U','T':'A'}
+    return ''.join([encoding_table[c] for c in dna_strand])
