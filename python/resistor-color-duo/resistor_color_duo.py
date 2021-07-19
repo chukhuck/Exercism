83391579e81c73e10b7b804c55e449e92bbf1ebc
Alex Potapchuk <chuk.huck@ya.ru>
2021-07-19 15:54:35 +0300

    python.resistorcolorduo.s1

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,31 @@
 def value(colors):
-    pass
+    pallete = [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        ]
+
+    return sum([pallete.index(color)*pow(10, 1-index) for index, color in enumerate(colors[:2])])
+
+def value1(colors):
+    pallete = [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        ] 
+
+    return int(str(pallete.index(colors[0])) + str(pallete.index(colors[1]))) 
