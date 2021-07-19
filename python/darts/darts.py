44dffbca8941a4c887ac4b1374a56d033112e9c5
Alex Potapchuk <chuk.huck@ya.ru>
2021-07-19 15:30:32 +0300

    python.darts.s1

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,20 @@
+ 
 def score(x, y):
-    pass
+    outer_radius = 10
+    middle_radius = 5
+    inner_radius = 1 
+    no_target_score = 0
+    outer_radius_score = 1
+    middle_radius_score = 5
+    inner_radius_score = 10
+
+    distance = pow(x*x+y*y, 0.5) 
+
+    if distance <= inner_radius:
+        return inner_radius_score
+    elif distance <= middle_radius:
+        return middle_radius_score
+    elif distance <= outer_radius:
+        return outer_radius_score
+    else:
+        return no_target_score
