49ac88f4cb0b3f02018645f382fea516d4f4cb58
Alex Potapchuk <chuk.huck@ya.ru>
2021-07-22 09:53:33 +0300

    csharp.resistorcolorduo.s1

--- csharp/resistor-color-duo/ResistorColorDuo.cs
+++ csharp/resistor-color-duo/ResistorColorDuo.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class ResistorColorDuo
 {
+    private static List<string> pallette = new List<string>
+    {
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white"
+    };
     public static int Value(string[] colors)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return pallette.IndexOf(colors[0])*10 + pallette.IndexOf(colors[1]) ;
     }
 }
